795d40f147b84be83d53d23092138b8495bcbb52
Jørgen Fjølstad <602622@stud.hvl.no>
2021-09-28 15:11:45 +0200

    del1

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -5,39 +5,73 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		System.out.print("[");
+		if (tabell.length != 0) {
+			int i;
+			for (i = 0; i < tabell.length - 1; i++) {
+				int t = tabell[i];
+				System.out.print(t + ",");
+
+			}
+			System.out.print(tabell[i]);
+		}
+		System.out.print("]");
 
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		throw new UnsupportedOperationException("reverser ikke implementert");
+
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
+		// Vanlig for-løkke
+		int sum = 0;
+		for (int i = 0; i < tabell.length; i++) {
+			sum += tabell[i];
+		}
+		sum = 0;
+		// Utvidet for-løkke
+		for (int i : tabell) {
+			sum += i;
+		}
+		sum = 0;
+		// While-løkke
+		int i = 0;
+		while (i < tabell.length) {
+			sum += tabell[i];
+			i++;
+		}
+
+		return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
+		for (int number : tabell) {
+			if (number == tall)
+				return true;
+		}
+
+		return false;
+
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
+		for (int i = 0; i < tabell.length; i++) {
+			if (tall == tabell[i]) {
+				return i;
+			}
+		}
 
+		return -1;
 	}
 
 	// f)
@@ -45,7 +79,7 @@ public class Tabeller {
 
 		// TODO
 		throw new UnsupportedOperationException("reverser ikke implementert");
-		
+
 	}
 
 	// g)
